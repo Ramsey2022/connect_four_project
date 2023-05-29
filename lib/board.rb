81e1dc9dd31a792148ab38965032504ab08abb78
Ramsey2022 <abovethelotus@yahoo.com>
2023-05-29 11:45:53 -0700

    completed game grid and tests

--- lib/board.rb
+++ lib/board.rb
@@ -1,3 +1,25 @@
 # frozen_string_literal: true
 
 require_relative 'custom_pieces'
+
+# class for game grid
+class Board
+  include Custom_pieces
+
+  attr_reader :grid
+
+  def initialize
+    @grid = Array.new(6) { Array.new(7) { empty_circle} }
+  end
+
+  def display_board
+    @grid.each do |row|
+      puts row.join(' ')
+    end
+    puts (1..7).to_a.join(' ')
+  end
+
+  def update_board(row, column, symbol)
+    @grid[row][column] = symbol
+  end
+end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,1 +1,38 @@
 require_relative '../lib/board'
+
+describe Board do
+  let(:yellow_circle) { described_class.new.yellow_circle }
+  let(:blue_circle) { described_class.new.blue_circle }
+
+  describe '#update_board' do
+    subject(:board_update) { Board.new }
+
+    context 'when 1st row and 1st column updated with yellow circle' do
+      before do
+        board_update.update_board(1, 1, yellow_circle)
+      end
+
+      it 'updates cell on board' do
+        board = board_update.instance_variable_get(:@grid)
+        updated_cell = board[1][1]
+        expect(updated_cell).to eq(yellow_circle)
+      end
+    end
+
+    context 'when 6th row and 1st column updated with blue circle' do
+      before do
+        board_update.update_board(5, 0, blue_circle)
+      end
+
+      it 'updates cell on board' do
+        board = board_update.instance_variable_get(:@grid)
+        updated_cell = board[5][0]
+        expect(updated_cell).to eq(blue_circle)
+      end
+    end
+  end
+
+  describe '#check_horizontal' do
+    
+  end
+end
