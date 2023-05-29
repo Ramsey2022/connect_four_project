6e2edc71c5bc952905073d1edeb66cf419949600
Ramsey2022 <abovethelotus@yahoo.com>
2023-05-29 14:11:08 -0700

    added players, verified inputs along with tests

--- lib/game.rb
+++ lib/game.rb
@@ -3,3 +3,57 @@
 require_relative 'board'
 require_relative 'player'
 require_relative 'custom_pieces'
+
+# class for game
+class Game
+  include Custom_pieces
+
+  def initialize
+    @board = Board.new
+    @player1 = Player.new(yellow_circle)
+    @player2 = Player.new(blue_circle)
+    @turn = 0
+  end
+
+  def new_game
+    set_player_name
+    loop do
+      current_player = player_turn
+      @board.display_board
+      column = player_input(current_player)
+      @board.update_board(@board.available_row(column), column, current_player.symbol)
+      break if game_over?(current_player) || draw?
+    end
+    @board.display_board
+  end
+
+  def player_input(player)
+    loop do
+      puts "#{player.symbol} #{player.name}'s turn."
+      puts 'Enter column number: '
+      column = gets.chomp.to_i - 1
+      return column if verify_input(column)
+
+      puts "Invalid input!/n/n"
+    end
+  end
+
+  def verify_input(input)
+    input.between?(0, 6) && !@board.full_column?(input)
+  end
+
+  def player_turn
+    @turn += 1
+    @turn.odd? ? @player1 : @player2
+  end
+
+  def ask_player_name(number)
+    puts "Enter player#{number} name: "
+    gets.chomp
+  end
+
+  def set_player_name
+    @player1.name = ask_player_name(1)
+    @player2.name = ask_player_name(2)
+  end
+end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -8,7 +8,7 @@ describe Game do
       it 'returns true' do
         input = 2
         result = valid_input.verify_input(input)
-        expect(result).to eq(2)
+        expect(result).to be(true)
       end
     end
 
@@ -26,12 +26,50 @@ describe Game do
       end
     end
 
-    context 'when player input not valid' do
+    context 'when player input greater than 7' do
       it 'returns false' do
-        invalid_input != (1..7)
+        invalid_input = 8
+        result = valid_input.verify_input(invalid_input)
+        expect(result).to be(false)
+      end
+    end
+
+    context 'when player input less than 1' do
+      it 'returns false' do
+        invalid_input = -2
         result = valid_input.verify_input(invalid_input)
         expect(result).to be(false)
       end
     end
   end
+
+  describe '#player_turn' do
+    subject(:verify_turn) { described_class.new }
+    let(:player1) { verify_turn.instance_variable_get(:@player1) }
+    let(:player2) { verify_turn.instance_variable_get(:@player2) }
+
+    before do
+      player1.name = 'Jeff'
+      player2.name = 'Sarah'
+    end
+
+    context 'when it is player1 turn' do
+      it 'returns player1' do
+        return_player = verify_turn.player_turn
+        expect(return_player.name).to eq(player1.name)
+      end
+    end
+
+    context 'when it is player2 turn' do
+      before do
+        verify_turn.instance_variable_set(:@turn, 1)
+      end
+
+      it 'returns player2' do
+        return_player = verify_turn.player_turn
+        expect(return_player.name).to eq(player2.name)
+      end
+    end
+  end
+
 end
